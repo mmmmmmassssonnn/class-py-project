f1251fd5a00f369fd042df6e08de9f7f2db16882
mmmmmmassssonnn <49329809+mmmmmmassssonnn@users.noreply.github.com>
2025-04-08 09:49:34 -0400

    init

---
+++ orderform.py
@@ -0,0 +1,9 @@
+class Order:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def orderFormat(self):
+        # do a return with the order formatted here
+
