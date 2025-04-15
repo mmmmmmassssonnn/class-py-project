862839452ec53b1975ac693857da8ade8da0c874
mmmmmmassssonnn <49329809+mmmmmmassssonnn@users.noreply.github.com>
2025-04-15 10:10:47 -0400

    feat: add a json menu

--- orderform.py
+++ orderform.py
@@ -2,6 +2,8 @@
 # Date:
 # Programmers:
 
+import json
+
 class Order:
     def __init__(self, price, order):
         self.price = price
@@ -9,15 +11,35 @@ class Order:
 
     def orderFormat(self):
         # do a return with the order formatted here
+        pass
 
 # Define Constants
 subtotal = 0 # Total Before Tax
-total = subtotal * 1.07 # Total After Tax
+total = subtotal * 1.07 # Total After
+full_order = []
 
-keepgoing = true # The loop should keep going
+keep_going = True # The loop should keep going
 
 # Potential items: Espresso, Latte, Cappuccino, Muffins, Croissants
 # Prices: 3, 4, 3.5, 2, 5
 # tax rate: 0.07
 # Discounts: Codes: 5OFF, SENIOR, SECRET
 # Payment method: Cash, Card
+
+with open('menu.json', 'r') as file:
+    data = json.load(file)
+
+def get_menu_item(menuitem):
+    option = next(
+        (item for item in data if item["shorthand"] == menuitem), None
+    )
+    if option:
+        return option["price"]
+    return None
+
+print(get_menu_item("Es"))
+
+#print(data["Espresso"])
+
+while keep_going == True:
+    pass
